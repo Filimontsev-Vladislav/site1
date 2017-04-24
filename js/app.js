c703cafaa448211c72e66b194e74708956ad2547
Vladislav Filimontsev <1438355@tut.by>
2017-04-24 19:27:37 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//console.log(React);
+//console.log(ReactDOM);
+//ReactDOM.render(
+//React.createElement('h1', null, 'Hello, world'), 
+//document.getElementById('root')
+//);
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
